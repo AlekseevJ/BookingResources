adaada8ae3922fcebfb2ef4a7e559834feceb225
AlekseevJ <alexeev-48@yandex.ru>
2025-04-09 13:11:21 +0300

    feat: add model interface

---
+++ app/Interfaces/IModelService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Interfaces;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
+
+interface IModelService
+{
+    public function findBy(int $limit): \Illuminate\Pagination\LengthAwarePaginator;
+
+    public function findById(int $id): Collection|Model;
+
+    public function create(array $params): Model;
+
+    public function update(int $id, array $params): Model;
+
+    public function destroy(int $id): bool;
+}
