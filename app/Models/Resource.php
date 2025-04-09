ed365304741db75ea236cf9bd07970648f20ac38
AlekseevJ <alexeev-48@yandex.ru>
2025-04-09 13:17:50 +0300

    feat: add resource model

---
+++ app/Models/Resource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Resource extends Model
+{
+    /** @use HasFactory<\Database\Factories\ResourceFactory> */
+    use HasFactory;
+    
+    protected $fillable = ['name', 'type', 'description'];
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
+}
