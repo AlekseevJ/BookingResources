4a48f503cc5b7dc8f6ee2e5d7a3d3e3cf2d6384c
AlekseevJ <alexeev-48@yandex.ru>
2025-04-09 15:51:21 +0300

    feat: add exception handler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,17 +3,59 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (NotFoundHttpException $e) {
+            return response()->json([
+                'message' => 'Resource not found.',
+            ], Response::HTTP_NOT_FOUND);
+        });
+
+        $exceptions->renderable(function (MethodNotAllowedHttpException $e) {
+            return response()->json([
+                'message' => 'Method not allowed.',
+            ], Response::HTTP_METHOD_NOT_ALLOWED);
+        });
+
+        $exceptions->renderable(function (ValidationException $e) {
+            return response()->json([
+                'message' => 'Validation failed.',
+                'errors' => $e->errors(),
+            ], Response::HTTP_BAD_REQUEST);
+        });
+
+        $exceptions->renderable(function (InvalidArgumentException $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], Response::HTTP_BAD_REQUEST);
+        });
+
+        $exceptions->reportable(function (Throwable $e) {
+            Log::error('An error occurred: ' . $e->getMessage(), [
+                'exception' => $e,
+                'trace' => $e->getTraceAsString(),
+            ]);
+        });
+
+        $exceptions->renderable(function (Throwable $e) {
+            return response()->json([
+                'message' => 'Internal server error.',
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        });
     })->create();
